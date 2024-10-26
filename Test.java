e5ffa5c4cf2bf0b405d4497a63f156667bb7b743
Henry <henry.karppinen1997@gmail.com>
2024-10-26 16:37:22 +0300

    Added properties to object

--- Test.java
+++ Test.java
@@ -42,10 +42,10 @@ public class Test{
     private static void TestCase(Scanner myScanner){
         test.setTestCase(true);
         
-        Investment investment1 = new Investment(false, 12, 1.5f, 10000, 0, "months", "month");
-        Investment investment2 = new Investment(false, 1, 18f, 10000, 0, "years", "year");
-        Investment investment3 = new Investment(true, 12, 1.5f, 10000, 0.15f, "months", "month");
-        Investment investment4 = new Investment(true, 1, 18f, 10000, 0.15f, "years", "year");
+        Investment investment1 = new Investment(false, 12, 1.5f, 10000, 0, "months", "month", 0f, 0f);
+        Investment investment2 = new Investment(false, 1, 18f, 10000, 0, "years", "year", 0f, 0f);
+        Investment investment3 = new Investment(true, 12, 1.5f, 10000, 0.15f, "months", "month", 0f, 0f);
+        Investment investment4 = new Investment(true, 1, 18f, 10000, 0.15f, "years", "year", 0f, 0f);
         Calculations.Calculate(myScanner, investment1);
         Calculations.Calculate(myScanner, investment2);
         Calculations.Calculate(myScanner, investment3);
